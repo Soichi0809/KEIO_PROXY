37fdb005f19e22cfdcd2cd0bfdfc38f212f3caea
SOICHIRO HATSUKAIWA <49380301+Soichi0809@users.noreply.github.com>
2025-09-28 20:30:49 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -4,16 +4,15 @@ chrome.action.onClicked.addListener((tab) => {
             const currentUrl = new URL(tab.url);
             const originalHostname = currentUrl.hostname;
 
-            // ドメインの"."を"-"に置換
+            if (originalHostname.endsWith(".kras.lib.keio.ac.jp")) {
+                console.log("すでにkras経由のURLのため、処理を中断します。");
+                return;
+            }
+
             const modifiedHostname = originalHostname.replace(/\./g, "-");
-
-            // 新しいホスト名を作成
             const newHostname = modifiedHostname + ".kras.lib.keio.ac.jp";
-
-            // 新しいURLを構築
             const newUrl = `https://${newHostname}${currentUrl.pathname}${currentUrl.search}${currentUrl.hash}`;
 
-            // 現在のタブを新しいURLに更新
             chrome.tabs.update(tab.id, { url: newUrl });
         } catch (e) {
             console.error("URLの処理中にエラーが発生しました:", e);
